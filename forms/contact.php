ef22dfde7264cfa142dab5aa2e78e1e6c849032c
SergeiTsipliaev <142197512+SergeiTsipliaev@users.noreply.github.com>
2023-12-14 12:42:01 +0800

    Добавил почту

--- forms/contact.php
+++ forms/contact.php
@@ -27,9 +27,9 @@
   );
   
 
-  $contact->add_message( $_POST['name'], 'Ваше Имя');
-  $contact->add_message( $_POST['email'], 'E-mail');
-  $contact->add_message( $_POST['message'], 'Сообщение', 10);
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
