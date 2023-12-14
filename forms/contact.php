55a1795ec3d93adb14c9d13dae2b22fdc60e3b07
SergeiTsipliaev <142197512+SergeiTsipliaev@users.noreply.github.com>
2023-12-14 11:22:28 +0800

    Добавил почту

--- forms/contact.php
+++ forms/contact.php
@@ -1,8 +1,7 @@
 <?php
 
-
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'serg-ciplaev@yandex.ru;
+  $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -13,18 +12,18 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
   $contact->smtp = array(
-    'host' = 'smtp.yandex.ru',
-    'username' = 'serg-ciplaev@yandex.ru',
-    'password' = 'serbeen@1991',
-    'port' = '465'
+    'host' => 'smtp.yandex.ru',
+    'username' => 'serg-ciplaev@yandex.ru',
+    'password' => 'serbeen@1991',
+    'port' => '465'
   );
   
 
