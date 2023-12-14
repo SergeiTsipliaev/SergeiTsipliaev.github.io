f812753f38cfea744825c1f88ee5ce8bec463ab8
SergeiTsipliaev <142197512+SergeiTsipliaev@users.noreply.github.com>
2023-12-14 11:16:49 +0800

    Добавил почту

--- forms/contact.php
+++ forms/contact.php
@@ -13,12 +13,12 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
+  $contact->Mailer = 'smtp'
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
   $contact->smtp = array(
     'host' => 'smtp.yandex.ru',
